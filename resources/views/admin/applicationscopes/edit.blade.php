383b84ac3fdcd6fad474d647f5906a11a6253e11
Carmen Vavra <carmen070976@gmail.com>
2022-09-05 23:40:08 +0200

    applicationscope section_short

--- resources/views/admin/applicationscopes/edit.blade.php
+++ resources/views/admin/applicationscopes/edit.blade.php
@@ -2,46 +2,52 @@
 
 @section('content')
 @if(isset($applicationscope))
-    <div>
+    <div class="container">
       <form action="{{ route('admin.applicationscope.update', $applicationscope->id) }}" method="post">
         @csrf
         @method('put')
-        <div class="row py-2">
-          <div class="col">
-            <label for="name">Name</label>
-            <input type="text" class="form-control" name="name" id="name" placeholder="Name" value="{{ $applicationscope->name }}">
-          </div>
-          <div class="col">
-            <label for="name_latin">Name Latein</label>
-            <input type="text" class="form-control" name="name_latin" id="name_latin" placeholder="Name Latein" value="{{ old('name_latin', $applicationscope->name_latin) }}">
-          </div>
-        </div>
-        <h6>Sektion</h6>
-        <div class="row py-2">
-          <div class="col">
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="radioApplicationSection" id="radioApplicationSectionPhysical" value="physical" @if(isset($applicationscope['section_short']) && strtoupper($applicationscope['section_short']) == 'P') checked @endif>
-              <label class="form-check-label" for="radioApplicationScopePhysical">
-                körperlich
-              </label>
+
+        <div class="card"">
+          <div class="card-body">
+            <h5 class="card-title">Anwendungsbereich</h5>
+            <div class="row py-2">
+              <div class="col">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" name="name" id="name" placeholder="Name" value="{{ $applicationscope->name }}">
+              </div>
+              <div class="col">
+                <label for="name_latin">Name Latein</label>
+                <input type="text" class="form-control" name="name_latin" id="name_latin" placeholder="Name Latein" value="{{ old('name_latin', $applicationscope->name_latin) }}">
+              </div>
             </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="radioApplicationSection" id="radioApplicationSectionMental" value="mental" @if(isset($applicationscope['section_short']) && strtoupper($applicationscope['section_short']) == 'M') checked @endif>
-              <label class="form-check-label" for="radioApplicationSectionMental">
-                psychisch
-              </label>
+            <h6>Sektion</h6>
+            <div class="row py-2">
+              <div class="col">
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="radioApplicationSection" id="radioApplicationSectionPhysical" value="physical" @if(isset($applicationscope['section_short']) && strtoupper($applicationscope['section_short']) == 'P') checked @endif>
+                  <label class="form-check-label" for="radioApplicationScopePhysical">
+                    körperlich
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="radioApplicationSection" id="radioApplicationSectionMental" value="mental" @if(isset($applicationscope['section_short']) && strtoupper($applicationscope['section_short']) == 'M') checked @endif>
+                  <label class="form-check-label" for="radioApplicationSectionMental">
+                    psychisch
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="radioApplicationSection" id="radioApplicationSectionDivers" value="divers" @if(isset($applicationscope['section_short']) && strtoupper($applicationscope['section_short']) == 'D') checked @endif>
+                  <label class="form-check-label" for="radioApplicationSectionDivers">
+                    sonstiges
+                  </label>
+                </div>
+              </div>
             </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="radioApplicationSection" id="radioApplicationSectionDivers" value="divers" @if(isset($applicationscope['section_short']) && strtoupper($applicationscope['section_short']) == 'D') checked @endif>
-              <label class="form-check-label" for="radioApplicationSectionDivers">
-                sonstiges
-              </label>
+            <div class="row py-2">
+              <div class="col">
+                <button type="submit" class="btn btn-dark">ändern</button>
+              </div>
             </div>
-          </div>
-        </div>
-        <div class="row py-2">
-          <div class="col">
-            <button type="submit" class="btn btn-dark">ändern</button>
           </div>
         </div>
       </form>
