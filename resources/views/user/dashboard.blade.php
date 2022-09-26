c44dbf771747ff7cb2ea453965d9060ed4abcd61
Carmen Vavra <carmen070976@gmail.com>
2022-09-27 01:04:17 +0200

    css

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -100,9 +100,15 @@
                 </div>
               </div>
               <div class="col-md-4">
-                <div class="card">
+                <div class="card bg-card-incredients">
                   <div class="card-body">
-                    <h5 class="card-title">Inhaltsstoffe</h5>
+                    <h5 class="card-title">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-box-arrow-in-down-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M6.364 2.5a.5.5 0 0 1 .5-.5H13.5A1.5 1.5 0 0 1 15 3.5v10a1.5 1.5 0 0 1-1.5 1.5h-10A1.5 1.5 0 0 1 2 13.5V6.864a.5.5 0 1 1 1 0V13.5a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5v-10a.5.5 0 0 0-.5-.5H6.864a.5.5 0 0 1-.5-.5z"/>
+                        <path fill-rule="evenodd" d="M11 10.5a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1 0-1h3.793L1.146 1.854a.5.5 0 1 1 .708-.708L10 9.293V5.5a.5.5 0 0 1 1 0v5z"/>
+                      </svg>
+                      Inhaltsstoffe
+                    </h5>
                     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="{{ route('admin.incredients.index') }}" class="card-link btn btn-sm btn-primary">Enter</a>
